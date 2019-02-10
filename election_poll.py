b05628c5b82b8e2fdb62706a1ce07c2912d9fd85
vinay <satyavinay456@gmail.com>
2019-02-10 20:27:26 +0530

    Update election_poll.py

--- election_poll.py
+++ election_poll.py
@@ -3,5 +3,5 @@ def election_polls(names_list):
 	return sorted([name for name,count in occurences_dict.items() if count==max(occurences_dict.values())],key=lambda x:x.lower())[-1]	
 
 if __name__=='__main__':
-	names=['vinay','bhanu','vinay','bhanu','vinay','anil','anil','anil',]
+	names=['vinay','bhanu','vinay','bhanu','vinay','anil','anil','anil']
 	print(election_polls(names))
