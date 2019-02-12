6f7ee8e00f4c5bbd6c4107b878794b28f549729c
vinay <satyavinay456@gmail.com>
2019-02-12 12:50:08 +0530

    Update sticks.py

--- sticks.py
+++ sticks.py
@@ -1,11 +1,15 @@
+#input the sticks with delimiter as space
+print("enter the sticks with delilmiter as space")
+sticks=list(map(int,input().split()))
+print("sticks entered : ",sticks)
+
 sticks_cut=[]
-sticks=[1, 1, 2, 3]
 sticks_cut.append(len(sticks))
+
 while True:
 	sticks=[i-min(sticks) for i in sticks if i-min(sticks)>0]
 	if len(sticks)!=0:
 		sticks_cut.append(len(sticks))
 	if sticks==[] : break
 
-print("final sticks :",sticks)
-print("sticks cut : ",sticks_cut)
+print("list of number of sticks you had at the start of each turn: ",sticks_cut)
