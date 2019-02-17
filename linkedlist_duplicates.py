a7d1423c3ce1ea5904b7e76026ca57ebc8cab681
vinay <satyavinay456@gmail.com>
2019-02-18 02:04:38 +0530

    Update linkedlist_duplicates.py

--- linkedlist_duplicates.py
+++ linkedlist_duplicates.py
@@ -2,12 +2,10 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
-
 class LinkedList:
     def __init__(self):
         self.head = None
         self.last_node = None
-
     def append(self, data):
         if self.last_node is None:
             self.head = Node(data)
@@ -15,26 +13,22 @@ class LinkedList:
         else:
             self.last_node.next = Node(data)
             self.last_node = self.last_node.next
-
     def previous_node(self, ref_node):
         current = self.head
         while (current and current.next != ref_node):
             current = current.next
         return current
-
     def remove(self, node):
         prev_node = self.previous_node(node)
         if prev_node is None:
             self.head = self.head.next
         else:
             prev_node.next = node.next
-
     def display(self):
         current = self.head
         while current:
             print(current.data, end='-->')
             current = current.next
-
 
 def remove_duplicates(llist):
     current1 = llist.head
@@ -47,10 +41,9 @@ def remove_duplicates(llist):
             current2 = current2.next
         current1 = current1.next
 
-l_list = LinkedList()
-
 if __name__ =='__main__':
-#input the numbers with delimiter as space
+    #input the numbers with delimiter as space
+    l_list = LinkedList()
     print("Enter the numbers with delilmiter as space")
     data_list=list(map(int,input().split()))
     print("list entered  :",data_list)
