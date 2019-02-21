2be6010fe96737359c3124e32e5ffe1b9de746e0
vinay <satyavinay456@gmail.com>
2019-02-22 01:47:25 +0530

    Add files via upload

---
+++ divisibility.py
@@ -0,0 +1,14 @@
+import itertools
+def divisibility(number):
+	return [int("".join(i)) for i in set(itertools.permutations(number)) if (int(''.join(i))/8)%2==0]
+
+if __name__=='__main__':
+	print("Enter the number to check divisibility")
+	number=input()
+	print("You entered",number)
+	print("permutations of entered number is ",set([int("".join(i)) for i in set(itertools.permutations(number))]))
+	print("All permutations of given digit that can be evenly divisible by 8 :",divisibility(number))
+	if divisibility(number)==[]:
+		print("Permutations of Entered number cannot be evenly divisible by 8")
+	else:
+		print("Permutations of Entered number can be evenly divisible by 8")
